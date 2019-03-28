8a2e95bf9621bef6fb1d592e556ff0134b5c7fd5
Spencer Jones <spencern.jones@faulkner.edu>
2019-03-28 16:53:11 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -40,6 +40,11 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    #if guess repeated
+    #  flash[:message] = "You have already used that letter."
+    #elseif invalid
+    #  flash[:message] = "Invalid guess."
+    #end
     redirect '/show'
   end
   
